6a4eb26d707ea54b8f850a5a823523e1a86cd8ab
flaviocarvalh0 <flavioalvespw@outlook.com>
2022-02-22 16:37:00 -0300

    DbContext Configurado

--- SmartSchool.API/Program.cs
+++ SmartSchool.API/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(
+    context => context.UseSqlite(builder.Configuration.GetConnectionString("default"))
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
